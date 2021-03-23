9b07c67e502ddbbfc91fef4113cbb82f1e6804a5
Fabian Florez <fdflorezp@correo.udistritale.edu.co>
2021-03-22 20:45:39 -0500

    redireccionamiento de la pagina

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,29 +14,44 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
-router.get('/servicios', (req, res) =>{
+router.get('/servicios', (req, res) => {
     res.render('pages/servicios');
 });
 
-router.get('/actualidad', (req, res) =>{
+router.get('/actualidad', (req, res) => {
     res.render('pages/actualidad');
 });
 
-router.get('/contacto', (req, res) =>{
+router.get('/contacto', (req, res) => {
     res.render('pages/contacto');
 });
 
 //Enviar Correo Electronico
 
-router.post('/send-data', (req, res) => {
-    const newContanct = {
-        nombre: req.body.name,
-        mail: req.body.mail,
-        telefono: req.body.phone,
-        mensaje: req.body.message
-    };
-    db.ref('posibles-clientes').push(newContanct);
-    res.redirect('/contacto');
+router.post('/send-data', async (req, res) => {
+    const { name, mail, phone, message } = req.body;
+    const errors = [];
+
+    if(!name || !mail || !phone || !message) {
+        errors.push({text: 'Por favor complete todos los datos para continuar'});
+    }
+    if(errors.length > 0){
+        res.render('pages/contacto', {
+            errors,
+            name
+        });
+    }
+    else {
+
+        const newContanct = {
+            nombre: req.body.name,
+            mail: req.body.mail,
+            telefono: req.body.phone,
+            mensaje: req.body.message
+        };
+        await db.ref('posibles-clientes').push(newContanct);
+        res.redirect('/');
+    }
 })
 
 module.exports = router;
